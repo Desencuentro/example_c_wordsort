c247d872c83df155053022ee33835322f366e4a9
desencuentro <eserra@fi.uba.ar>
2022-01-20 03:27:24 -0300

    +onEmptyLineFile_sortfileReadword_isNull_andErrorIsNone,+onWordFile_sortfileReadword_isWord_andErrorIsNone

--- test/sortfile_tests.c
+++ test/sortfile_tests.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "sortfile_tests.h"
 #include "smalltests.h"
 #include "../src/sortfile.h"
@@ -31,6 +32,41 @@ void onEmptyFile_sortfileReadword_isNull_andErrorIsNone()
     fclose(f);
 }
 
+void onEmptyLineFile_sortfileReadword_isNull_andErrorIsNone()
+{
+    FILE *f = fopen("test/testfiles/emptyLine.txt", "r");
+    errorcode_t error = ERROR_UNKNOWN;
+
+    char* word = sortfile_readword(f, &error);
+
+    assertEquals("On empty line file, sortfile_readword returns NULL.",
+        NULL, word);
+    assertEquals("On empty line file, sortfile_readword error is ERROR_NONE.",
+        ERROR_NONE, error);
+
+    free(word);
+    fclose(f);
+}
+
+void onWordFile_sortfileReadword_isWord_andErrorIsNone()
+{
+    FILE *f = fopen("test/testfiles/wordNoLine.txt", "r");
+    errorcode_t error = ERROR_UNKNOWN;
+
+    char* word = sortfile_readword(f, &error);
+
+    assertNotEquals("On file with word, sortfile_readword returns not NULL.",
+        NULL, word);
+    int cmp = (NULL == word) ? 0x1986 : strcmp("word", word);
+    assertEquals("On file with word, strcmp sortfile_readword with word is 0.",
+        0, cmp);
+    assertEquals("On file with word, sortfile_readword error is ERROR_NONE.",
+        ERROR_NONE, error);
+
+    free(word);
+    fclose(f);
+}
+
 void sortfile_tests()
 {
     printf("\n\n");
@@ -40,4 +76,6 @@ void sortfile_tests()
 
     onNullFile_sortfileReadword_isNull_andErrorIsArgs();
     onEmptyFile_sortfileReadword_isNull_andErrorIsNone();
+    onEmptyLineFile_sortfileReadword_isNull_andErrorIsNone();
+    onWordFile_sortfileReadword_isWord_andErrorIsNone();
 }
--- test/sortfile_tests.h
+++ test/sortfile_tests.h
@@ -5,5 +5,7 @@ void sortfile_tests();
 
 void onNullFile_sortfileReadword_isNull_andErrorIsArgs();
 void onEmptyFile_sortfileReadword_isNull_andErrorIsNone();
+void onEmptyLineFile_sortfileReadword_isNull_andErrorIsNone();
+void onWordFile_sortfileReadword_isWord_andErrorIsNone();
 
 #endif
