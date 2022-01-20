d601cc9f33867d2bf152911d6e836d2cbae1a997
desencuentro <eserra@fi.uba.ar>
2022-01-20 03:18:07 -0300

    ~sortfile_readword: if NULL == input, *result = ERROR_ARGS

--- src/sortfile.c
+++ src/sortfile.c
@@ -1,9 +1,16 @@
 #include "sortfile.h"
 
+#define READSIZE ((size_t) 8)
+
 char* sortfile_readword(FILE* input, errorcode_t *result)
 {
-    if(result) {
-        (*result) = ERROR_ARGS;
+    errorcode_t error = ERROR_NONE;
+    if(NULL == input) {
+        error = ERROR_ARGS;
+    }
+
+    if(NULL != result) {
+        (*result) = error;
     }
     return NULL;
 }
