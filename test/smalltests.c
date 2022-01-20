fca94df7e27d7c198a8b9e59ec2c493c77424897
desencuentro <eserra@fi.uba.ar>
2022-01-19 22:36:30 -0300

    +assertEquals, +smalltests_do_pass, +smalltests_do_start_test

--- test/smalltests.c
+++ test/smalltests.c
@@ -4,15 +4,36 @@
 int _smalltests_passed = 0;
 int _smalltests_total = 0;
 
+void _assertEquals(char* message, uintmax_t expected, uintmax_t actual)
+{
+    smalltests_do_start_test(message);
+    if(expected == actual) {
+        smalltests_do_pass();
+    } else {
+        printf("FAILED, expected %lx, actual %lx\n", (long unsigned) expected, (long unsigned) actual);
+    }
+}
+
 void _assertNotEquals(char* message, uintmax_t unexpected, uintmax_t actual)
 {
-    printf("%s... ",message);
+    smalltests_do_start_test(message);
     if(unexpected != actual) {
-        _smalltests_passed++;
-        printf("Ok.\n");
+        smalltests_do_pass();
     } else {
         printf("FAILED, unexpected %lx\n", (long unsigned) actual);
     }
+}
+
+
+void smalltests_do_pass()
+{
+    _smalltests_passed++;
+    printf("Ok.\n");
+}
+
+void smalltests_do_start_test(char* message)
+{
+    printf("%s... ",message);
     _smalltests_total++;
 }
 
--- test/smalltests.h
+++ test/smalltests.h
@@ -3,8 +3,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
-/** Shows a summary of tests that passed vs. tests executed. **/
-void smalltests_summary();
+#define assertEquals(message, expected, actual) _assertEquals(message, (uintmax_t) expected, (uintmax_t) actual)
+/**
+ * Assert that the `actual` and `expected` values are equal.
+ * @param message Assertion message to display.
+ * @param expeted Expected value.
+ * @param actual Actual value.
+ **/
+void _assertEquals(char* message, uintmax_t expected, uintmax_t actual);
 
 #define assertNotEquals(message, unexpected, actual) _assertNotEquals(message, (uintmax_t) unexpected, (uintmax_t) actual)
 /**
@@ -14,5 +20,17 @@ void smalltests_summary();
  * @param actual Actual value.
  **/
 void _assertNotEquals(char* message, uintmax_t unexpected, uintmax_t actual);
+
+/** Passes and ends the currently started test with `smalltests_do_start_test`. **/
+void smalltests_do_pass();
+
+/**
+ * Starts a tests with the given `message`.
+ * @param message Should not be NULL.
+ **/
+void smalltests_do_start_test(char* message);
+
+/** Shows a summary of tests that passed vs. tests executed. **/
+void smalltests_summary();
 
 #endif
