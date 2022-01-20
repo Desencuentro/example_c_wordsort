aa8704e5a7cd4b08cd9f3573fdbf3a2168201336
desencuentro <eserra@fi.uba.ar>
2022-01-19 22:15:14 -0300

    +assertNotEquals

--- test/smalltests.c
+++ test/smalltests.c
@@ -4,6 +4,18 @@
 int _smalltests_passed = 0;
 int _smalltests_total = 0;
 
+void _assertNotEquals(char* message, uintmax_t unexpected, uintmax_t actual)
+{
+    printf("%s... ",message);
+    if(unexpected != actual) {
+        _smalltests_passed++;
+        printf("Ok.\n");
+    } else {
+        printf("FAILED, unexpected %lx\n", (long unsigned) actual);
+    }
+    _smalltests_total++;
+}
+
 void smalltests_summary()
 {
     printf("**************** TESTS SUMMARY: %i/%i. ****************\n",
--- test/smalltests.h
+++ test/smalltests.h
@@ -1,7 +1,18 @@
 #ifndef __SMALLTESTS_H__
 #define __SMALLTESTS_H__
+#include <stddef.h>
+#include <stdint.h>
 
 /** Shows a summary of tests that passed vs. tests executed. **/
 void smalltests_summary();
+
+#define assertNotEquals(message, unexpected, actual) _assertNotEquals(message, (uintmax_t) unexpected, (uintmax_t) actual)
+/**
+ * Assert that the `actual` and `unexpected` values are not equal.
+ * @param message Assertion message to display.
+ * @param expeted Value not expected.
+ * @param actual Actual value.
+ **/
+void _assertNotEquals(char* message, uintmax_t unexpected, uintmax_t actual);
 
 #endif
