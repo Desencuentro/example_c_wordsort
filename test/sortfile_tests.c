f3182eacf1c033adc31cb2dfbf3a5bab47f06f20
desencuentro <eserra@fi.uba.ar>
2022-01-20 03:17:04 -0300

    +onEmptyFile_sortfileReadword_isNull_andErrorIsNone

--- test/sortfile_tests.c
+++ test/sortfile_tests.c
@@ -6,13 +6,29 @@
 
 void onNullFile_sortfileReadword_isNull_andErrorIsArgs()
 {
-    errorcode_t error = ERROR_NONE;
+    errorcode_t error = ERROR_UNKNOWN;
     char* word = sortfile_readword(NULL, &error);
     assertEquals("On null file, sortfile_readword returns NULL.",
         NULL, word);
     assertEquals("On null file, sortfile_readword error is ERROR_ARGS.",
         ERROR_ARGS, error);
     free(word);
+}
+
+void onEmptyFile_sortfileReadword_isNull_andErrorIsNone()
+{
+    FILE *f = fopen("test/testfiles/empty.txt", "r");
+    errorcode_t error = ERROR_UNKNOWN;
+
+    char* word = sortfile_readword(f, &error);
+
+    assertEquals("On empty file, sortfile_readword returns NULL.",
+        NULL, word);
+    assertEquals("On empty file, sortfile_readword error is ERROR_NONE.",
+        ERROR_NONE, error);
+
+    free(word);
+    fclose(f);
 }
 
 void sortfile_tests()
@@ -23,4 +39,5 @@ void sortfile_tests()
     printf("****************************************************************\n\n");
 
     onNullFile_sortfileReadword_isNull_andErrorIsArgs();
+    onEmptyFile_sortfileReadword_isNull_andErrorIsNone();
 }
--- test/sortfile_tests.h
+++ test/sortfile_tests.h
@@ -4,5 +4,6 @@
 void sortfile_tests();
 
 void onNullFile_sortfileReadword_isNull_andErrorIsArgs();
+void onEmptyFile_sortfileReadword_isNull_andErrorIsNone();
 
 #endif
