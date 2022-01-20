c12ee1ed26976f9d183561dd21605227ff95aaf0
desencuentro <eserra@fi.uba.ar>
2022-01-19 22:15:33 -0300

    +list_new, +list_free

--- test/list_tests.c
+++ test/list_tests.c
@@ -1,7 +1,14 @@
 #include "list_tests.h"
 #include "smalltests.h"
 
+void listNew_isNotNull()
+{
+    list_t* list = list_new();
+    assertNotEquals("list_new() is not NULL", NULL, list);
+    list_free(list);
+}
+
 void list_tests()
 {
-
+    listNew_isNotNull();
 }
--- test/list_tests.h
+++ test/list_tests.h
@@ -1,6 +1,9 @@
 #ifndef __LIST_TESTS_H__
 #define __LIST_TESTS_H__
+#include "../src/list.h"
 
 void list_tests();
+
+void listNew_isNotNull();
 
 #endif
